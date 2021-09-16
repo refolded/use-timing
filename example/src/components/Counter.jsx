16688c0c05a450ed18c0b0c4a3c6b969f48b5d72
Ali Raslan <24810123+raslan@users.noreply.github.com>
2021-09-16 22:20:19 +0200

    feat: add useInterval example

---
+++ example/src/components/Counter.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useInterval } from "use-timing";
+
+const Counter = () => {
+  const [delay, setDelay] = useState(1000);
+
+  const [count, setCount] = useState(0);
+
+  const [start, stop] = useInterval(() => {
+    setCount(count + 1);
+  }, delay);
+
+  return (
+    <div className='py-12 flex flex-col justify-center font-mono w-full m-auto h-screen items-center'>
+      <h1 className='text-6xl font-semibold text-gray-400'>useInterval</h1>
+      <h2 className='font-bold text-9xl py-4 text-green-500 my-2'>
+        Count: {count}
+      </h2>
+      <div className='flex flex-col w-1/4 justify-center gap-2'>
+        <label
+          htmlFor='#useInterval'
+          className='text-2xl text-gray-400 font-semibold'
+        >
+          Delay:
+        </label>
+        <input
+          className='text-2xl rounded-full max-w-3xl w-full text-black p-4 my-3 text-center bg-green-100'
+          id='useInterval'
+          value={delay}
+          onChange={(e) => setDelay(+e.target.value)}
+        />
+      </div>
+      <div className='grid grid-cols-2 gap-3 py-10'>
+        <button
+          className='py-6 font-mono text-xl bg-green-800 px-10 rounded-full text-white font-bold leading-relaxed tracking-wider max-w-lg'
+          onClick={() => {
+            start();
+          }}
+        >
+          Start Count
+        </button>
+        <button
+          className='py-6 font-mono text-xl border-2 border-gray-400 px-10 rounded-full text-white font-bold leading-relaxed tracking-wider max-w-lg'
+          onClick={() => {
+            stop();
+          }}
+        >
+          Stop Count
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Counter;
