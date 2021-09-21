0c80bb2d1775f6101203604f97b0e5c1b35fc4c2
Ali Raslan <24810123+raslan@users.noreply.github.com>
2021-09-21 23:10:28 +0200

    feat: export new hooks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { useInterval } from "./useInterval";
 export { useTimeout } from "./useTimeout";
 export { useCycle } from "./useCycle";
+export { useInOut } from "./useInOut";
+export { useCountdown } from "./useCountdown";
