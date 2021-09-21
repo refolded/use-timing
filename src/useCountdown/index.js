9df697712618e902810f668b1d709975606ae3fd
Ali Raslan <24810123+raslan@users.noreply.github.com>
2021-09-21 23:10:36 +0200

    feat: add useCountdown hook

---
+++ src/useCountdown/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { useInterval } from "../useInterval/index";
+
+export const useCountdown = (inputDelay) => {
+  const [currentTime, setCurrentTime] = useState(inputDelay);
+
+  const [start, stop] = useInterval(() => {
+    setCurrentTime(currentTime - 1000);
+  }, 1000);
+
+  useEffect(() => {
+    if (currentTime === 0) stop();
+  }, [currentTime]);
+
+  useEffect(() => {
+    setCurrentTime(inputDelay);
+  }, [inputDelay]);
+
+  return [start, stop, currentTime];
+};
