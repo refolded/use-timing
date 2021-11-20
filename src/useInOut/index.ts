bf970db8337a91d887256dde191c9bd69a5af11a
Ak <abdallahakrab98@gmail.com>
2021-11-20 20:33:50 +0200

    feat: migrate useInOut to typescript

--- src/useInOut/index.ts
+++ src/useInOut/index.ts
@@ -1,9 +1,12 @@
-import { useCallback, useRef, useState } from "react";
-import { useTimeout } from "../useTimeout/index";
+import { useCallback, useRef, useState } from 'react';
+import { useTimeout } from '../useTimeout/index';
 
-export const useInOut = ([firstCallback, secondCallback], delay) => {
-  const initialCallback = useRef(firstCallback);
-  const endingCallback = useRef(secondCallback);
+export const useInOut = (
+  [firstCallback, secondCallback]: [Function, Function],
+  delay: number,
+) => {
+  const initialCallback = useRef<Function>(firstCallback);
+  const endingCallback = useRef<Function>(secondCallback);
   const [internalDelay, setInternalDelay] = useState(delay);
   const [timeoutStart] = useTimeout(endingCallback.current, internalDelay);
   const start = useCallback(() => {
