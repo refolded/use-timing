265cdddb6db4112d0a22ec56458488f14c1fd854
Ak <abdallahakrab98@gmail.com>
2021-11-21 10:50:13 +0200

    feat: migrate index to typescript

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { useInterval } from "./useInterval";
-export { useTimeout } from "./useTimeout";
-export { useCycle } from "./useCycle";
-export { useInOut } from "./useInOut";
-export { useCountdown } from "./useCountdown";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { useInterval } from './useInterval';
+export { useTimeout } from './useTimeout';
+export { useCycle } from './useCycle';
+export { useInOut } from './useInOut';
+export { useCountdown } from './useCountdown';
