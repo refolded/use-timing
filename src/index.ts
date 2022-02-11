af2768e02352322f66fd95b1c5901c4d32642ec4
raslan <24810123+raslan@users.noreply.github.com>
2022-02-11 20:44:35 +0200

    feat: add useDebounce hook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export { useInterval } from './useInterval';
-export { useTimeout } from './useTimeout';
-export { useCycle } from './useCycle';
-export { useInOut } from './useInOut';
-export { useCountdown } from './useCountdown';
+export { useCountdown } from "./useCountdown";
+export { useCycle } from "./useCycle";
+export { useDebounce } from "./useDebounce";
+export { useInOut } from "./useInOut";
+export { useInterval } from "./useInterval";
+export { useTimeout } from "./useTimeout";
