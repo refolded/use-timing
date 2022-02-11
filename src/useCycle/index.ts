f8f93bc59fe27655cf8e15650448a92f52ab25fb
raslan <24810123+raslan@users.noreply.github.com>
2022-02-11 20:45:12 +0200

    feat: update useCycle typescript implementation

--- src/useCycle/index.ts
+++ src/useCycle/index.ts
@@ -1,22 +1,17 @@
-import { useRef, useEffect, useCallback } from 'react';
-import { useInterval } from '../useInterval/index';
+import { useCallback, useEffect, useRef } from "react";
+import { useInterval } from "../useInterval/index";
 
-/**
- *
- * @param {number} index: the current index
- * @param {Array<number>} array: the array of delays
- * @returns {number} newIndex: the new index in the given array, or 0 for single element arrays.
- */
 const computeIndex = (index: number, array: number[]) =>
   array.length > 1 ? (index + 1) % array.length : 0;
 
-/**
- *
- * @param {Function} callback: Callback function to be looped.
- * @param {Array<number>} delays: Array of delays (in milliseconds) to cycle through.
- * @returns {Array} [start, stop, current delay].
- */
-export const useCycle = (callback: Function, delays: number[]) => {
+export const useCycle = (
+  callback: Function,
+  delays: number[]
+): {
+  start: () => void;
+  stop: () => void;
+  currentDelay: number;
+} => {
   // Define the delay and its starting index
   const delayIndex = useRef(0);
   const delay = useRef(delays[delayIndex.current]);
@@ -28,7 +23,7 @@ export const useCycle = (callback: Function, delays: number[]) => {
   }, []);
 
   // Create the interval
-  const [start, stop] = useInterval(() => {
+  const { start, stop } = useInterval(() => {
     shift();
     callback();
   }, delay.current);
@@ -39,5 +34,5 @@ export const useCycle = (callback: Function, delays: number[]) => {
   }, [delays]);
 
   // Return the interval control functions and current delay
-  return [start, stop, delay.current];
+  return { start, stop, currentDelay: delay.current };
 };
